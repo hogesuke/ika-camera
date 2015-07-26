df8ad08063ab56dfa49cbd492dec0c9c1d22d0df
hogesuke <miyado@gmail.com>
2015-07-27 03:40:53 +0900

    uploadする処理を追加

--- ikacamera.py
+++ ikacamera.py
@@ -6,6 +6,8 @@ import cv2
 import numpy as np
 import io
 import os
+import urllib2
+import MultipartPostHandler
 from datetime import datetime
 
 def setup():
@@ -42,15 +44,20 @@ def detect(camera, cascade_win, cascade_lose):
         win_rects = cascade_win.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
         lose_rects = cascade_lose.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
 
+        wait_time = datetime.now()
+
         if len(win_rects) > 0:
             win_frame_count += 1
 
             for rect in win_rects:
                 cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (255, 255, 255), thickness=2)
 
-            if win_frame_count > 5:
+            if win_frame_count > 3 and wait_time < datetime.now():
                 win_frame_count = 0
-                capture(camera, dist)
+                wait_time = wait_time + datetime.timedelta(seconds=10)
+                now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                path = capture(camera, dist, now)
+                send(path, 'win', now)
 
         if len(lose_rects) > 0:
             lose_frame_count += 1
@@ -58,9 +65,12 @@ def detect(camera, cascade_win, cascade_lose):
             for rect in lose_rects:
                 cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 0, 0), thickness=2)
 
-            if lose_frame_count > 5:
+            if lose_frame_count > 3 and wait_time < datetime.now():
                 lose_frame_count = 0
-                capture(camera, dist)
+                wait_time = wait_time + datetime.timedelta(seconds=10)
+                now = datetime.now()
+                path = capture(camera, dist, now.strftime('%Y%m%d%H%M%S'))
+                send(path, 'win', now.strftime('%Y-%m-%d %H:%M:%S'))
 
         cv2.imshow(window_name, frame)
 
@@ -68,11 +78,17 @@ def detect(camera, cascade_win, cascade_lose):
         if key == 27:
             break
 
-def capture(camera, dist):
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
+def capture(camera, dist, now):
+    path = '{0}/ika_result{1}.jpg'.format(dist, now)
     camera.resolution = (640, 480)
-    camera.capture('{0}/ika_result{1}.jpg'.format(dist, now))
+    camera.capture(path)
     camera.resolution = (160, 120)
+    return path
+
+def send(path, result, now):
+    opener = urllib2.build_opener(MultipartPostHandler.MultipartPostHandler)
+    params = {'result': result, 'datetime': now, 'file': open(path, 'rb')}
+    opener.open('http://ikashot.net/upload', params)
 
 def main():
 
